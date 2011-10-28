499674a41e79a4a0f40e5cd18c8837ad33b83144
Grégory Marcilhacy <g.marcilhacy@gmail.com>
2011-10-28 23:22:00 +0200

    Adding more robust test suite

--- test/bump_spec.rb
+++ test/bump_spec.rb
@@ -2,6 +2,19 @@ require File.dirname(__FILE__) + "/../lib/bump.rb"
 
 describe Bump do
   
+  before(:each) do
+    gemspec = %(Gem::Specification.new do |s|
+      s.version = "1.0.0"
+    end)
+    path = File.expand_path("fixture.gemspec","fixture/") 
+    File.open(path, 'w') {|f| f.write(gemspec) }
+  end
+
+  after(:each) do
+    path = File.expand_path("fixture.gemspec","fixture/") 
+    File.open(path, 'w') {|f| f.write("") }
+  end
+
   it "should find current version" do
     bump = Bump::Bump.new("current")
     bump.gemspec_path = File.dirname(__FILE__) + "/fixture/fixture.gemspec"
