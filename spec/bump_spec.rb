0fe1f469a1cfd74783e606c541a6405cb319d85e
grosser <grosser.michael@gmail.com>
2012-10-07 09:55:16 -0700

    add gemfile and make rake run the tests

--- spec/bump_spec.rb
+++ spec/bump_spec.rb
@@ -1,17 +1,16 @@
 require File.dirname(__FILE__) + "/../lib/bump.rb"
 
 describe Bump do
-  
-  before(:each) do
-    gemspec = %(Gem::Specification.new do |s|
-      s.version = "1.0.0"
-    end)
-    path = File.expand_path("fixture.gemspec","fixture/") 
-    File.open(path, 'w') {|f| f.write(gemspec) }
-  end
 
-  after(:each) do
-    path = File.expand_path("fixture.gemspec","fixture/") 
+  around do |example|
+    gemspec = <<-RUBY.sub(" "*6, "")
+      Gem::Specification.new do |s|
+        s.version = "1.0.0"
+      end
+    RUBY
+    path = File.expand_path("../fixture/fixture.gemspec", __FILE__)
+    File.open(path, 'w') {|f| f.write(gemspec) }
+    example.call
     File.open(path, 'w') {|f| f.write("") }
   end
 
