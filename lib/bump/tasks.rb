fbcbd65b73ff344e12abfbe732b683c610f54739
Steve Valaitis <steve@digitalnothing.com>
2013-03-26 21:41:29 -0700

    Use lambda instead of function in rake tasks

--- lib/bump/tasks.rb
+++ lib/bump/tasks.rb
@@ -1,6 +1,12 @@
 require "bump"
 
 namespace :bump do
+  run_bump = lambda do |bump, options|
+    output, status = Bump::Bump.run(bump, options)
+    puts output
+    abort unless status == 0
+  end
+
   (Bump::Bump::BUMPS + ["current"]).each do |bump|
     if bump == "current"
       desc "Show current gem version"
@@ -9,20 +15,12 @@ namespace :bump do
     end
 
     task bump do
-      run_bump(bump)
+      run_bump.call(bump, {})
     end
   end
 
   desc "Sets the version number using the VERSION environment variable"
   task :set do
-    run_bump("set", :version => ENV['VERSION'])
+    run_bump.call("set", :version => ENV['VERSION'])
   end
 end
-
-def run_bump(bump, options = {})
-  output, status = Bump::Bump.run(bump, options)
-  puts output
-  abort unless status == 0
-end
-
-
