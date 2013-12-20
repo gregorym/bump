514246703efa94ef35d1e4961dc2e9440f33d016
Jared Fraser <dev@jsf.io>
2013-12-20 16:13:43 +0000

    Add documentation to chef and gemspec finders

--- lib/bump/finders/chef.rb
+++ lib/bump/finders/chef.rb
@@ -1,6 +1,7 @@
 module Bump
   module Finders
-    class Chef < Bump::Finders::Finder
+    class Chef < Bump::Finders::GenericFile
+      # Finds a chef compatible version string
       def version
         File.read(file).match(/^version\s+(['"])(#{version_regex})['"]/)[0]
       end
--- lib/bump/finders/gemspec.rb
+++ lib/bump/finders/gemspec.rb
@@ -1,6 +1,7 @@
 module Bump
   module Finders
-    class Gemspec < Bump::Finders::Finder
+    class Gemspec < Bump::Finders::GenericFile
+      # Finds a compatible version in the gemspec file
       def version
         gemspec = File.read(file)
         gemspec[/\.version\s*=\s*["']#{version_regex}["']/, 1] ||
